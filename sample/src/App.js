71c64c7f083a17ad1da3320f3800b5c85d75f64d
Ali Soltani <soltani1384@gmail.com>
2021-03-01 17:57:27 +0330

    adding favicon

--- sample/src/App.js
+++ sample/src/App.js
@@ -1,5 +1,9 @@
+
 import logo from './logo.svg';
 import './App.css';
+
+import Button from '@material-ui/core/Button';
+
 
 function App() {
   return (
@@ -9,6 +13,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Button varient="contained">click</Button>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
